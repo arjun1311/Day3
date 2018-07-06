2a7a6c6dd9d3604ee1f3373680c534d075d3e8c7
vinay72 <vinaysagar971995@gmail.com>
2018-07-07 01:58:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Day3
+<h1>Sign-in and Sign-up page for Blood Donation Platform</h1>
+<h2>Front-end</h2>
---
+++ myscript.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems, options);
+});
+$(".button-collapse").sideNav();
+$(document).ready(function(){
+$('.sidenav').sidenav();
+});
